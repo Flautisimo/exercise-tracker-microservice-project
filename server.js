3959ca37464af16ab7c17d6482744365ea747c30
Glitch (innovative-diligent-scarf) <none>
2021-03-30 08:28:26 +0000

    🐥💝 Checkpoint ./server.js:30084160/356

--- server.js
+++ server.js
@@ -16,12 +16,22 @@ app.get('/', (req, res) => {
 let uri = 'mongodb+srv://user2:' + process.env.PW + '@cluster0.cakja.mongodb.net/fccExerciseTracker?retryWrites=true&w=majority';
 mongoose.connect(uri, { useNewUrlParser: true , useUnifiedTopology: true });
 
-// Create Schema
+// Create Schema for the exercise sessions
 let exerciseSessionSchema = new mongoose.Schema({
   description: {type: String, required: true},
   duration: {type: Number, required: true},
   date: String
-})
+});
+
+// Create Schema for the users
+let userSchema = new mongoose.Schema({
+  username: {type: String, required: true},
+  log: [exerciseSessionSchema]
+});
+
+// Create Models for the exercise sessions and users
+let Session = mongoose.model('Session', exerciseSessionSchema);
+let User = mongoose.model('User', userSchema);
 
 
 
