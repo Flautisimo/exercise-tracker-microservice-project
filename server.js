bd6981bf74a6529e11219b402c85820cd84efb8a
Glitch (innovative-diligent-scarf) <none>
2021-03-31 18:54:30 +0000

    🍎🎍 Checkpoint ./server.js:30084160/262

--- server.js
+++ server.js
@@ -94,11 +94,10 @@ app.get('/api/exercise/log', (req, res) => {
   // Find user by id using a query
   User.findById(req.query.userId, (err, data) => {
     if (!err) {
-      let responseObject = {};
-      data['count'] = 1;
-      responseObject = data;
+      let responseObject = {data};
       responseObject['count'] = data.log.length;
-      res.json(responseObject);
+      let result = responseObject.data;
+      res.json(result);
     }
   })
 });
