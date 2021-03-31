5451f6d0945e03306cbe3e2acada4edd03ab0caa
Glitch (innovative-diligent-scarf) <none>
2021-03-31 18:31:36 +0000

    👋🍘 Checkpoint ./server.js:30084160/187

--- server.js
+++ server.js
@@ -94,8 +94,8 @@ app.get('/api/exercise/log', (req, res) => {
   // Find user by id using a query
   User.findById(req.query.userId, (err, data) => {
     if (!err) {
-      data['count'] = 0
-      res.json(data)
+      data['count'] = ''
+      console.log(data.length)
       /*let responseObject = data;
       responseObject['count'] = data.log.length;
       res.json(responseObject);*/
