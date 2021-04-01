a90e35292702a5e7adbefbd4a756d89d30d58906
Glitch (innovative-diligent-scarf) <none>
2021-04-01 08:05:56 +0000

    🏦💰 Checkpoint ./server.js:30084160/1

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.get('/api/exercise.log', (req, res) => {
       const username = data.username;
       console.log({'from': from, 'to': to, 'limit': limit});
       
-      Session.find({userId}, {date: {$gte: new Date(from), $lte: new Date(to)}}).select(['id', 'description', 'duration', 'date']).limit(+limit).exec( (err, data) => {
+      Session.find({userId}, {date: {$gte: new Date(from), $lte: new Date(to)}}).select(['_id', 'description', 'duration', 'date']).limit(+limit).exec( (err, data) => {
         let customdata = data.map(session => {
           let formattedDate = new Date(session.date).toDateString();
           return {id: session.id, description: session.description, duration: session.duration, date: formattedDate};
